652c7cdb61180bb20052cec90be2b3de5a46882c
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-09-22 19:04:56 -1000

    more stuff

---
+++ js/mail-controller.js
@@ -0,0 +1,7 @@
+(function() {
+	window.onload = function(){
+        // ALL OF YOUR JAVASCRIPT CODE SHOULD GO HERE. 
+        // We have to use window.onload so your JavaScript doesn't execute until the page has loaded and all HTML has been downloaded to your browser
+        console.dir(window.geemails);
+      };
+})();
